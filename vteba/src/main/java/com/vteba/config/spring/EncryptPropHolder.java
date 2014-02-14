c22c5d34f60edc2c3b20e7731640edf3854b51f9
skmbw <tongku2008@126.com>
2014-02-14 14:21:26 +0800

    增加获取静态属性的方法，已经更改名字

--- vteba/src/main/java/com/vteba/config/spring/EncryptPropHolder.java
+++ vteba/src/main/java/com/vteba/config/spring/EncryptPropHolder.java
@@ -1,32 +1,37 @@
 package com.vteba.config.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 import com.vteba.util.cryption.DESUtils;
 
 /**
  * Properties属性密文配置
- * @author yinlei 
- * date 2012-7-16 下午4:28:33
+ * 
+ * @author yinlei date 2012-7-16 下午4:28:33
  */
-public class EncryptPropHolderConfigurer extends PropertyPlaceholderConfigurer {
+public class EncryptPropHolder extends PropertyPlaceholderConfigurer {
 	private String[] encryptPropNames = { "jdbc.username", "jdbc.password" };
+	private static Map<String, String> propertiesMap = new HashMap<String, String>();
 	
-	public EncryptPropHolderConfigurer() {
+	public EncryptPropHolder() {
 		super();
 	}
-	
+
 	@Override
 	protected String convertProperty(String propertyName, String propertyValue) {
 		if (isEncryptProp(propertyName)) {
 			String decryptValue = DESUtils.getDecryptString(propertyValue);
 			return decryptValue;
 		} else {
+			propertiesMap.put(propertyName, propertyValue);
 			return super.convertPropertyValue(propertyValue);
 		}
-		
-    }
-	
+
+	}
+
 	private boolean isEncryptProp(String propName) {
 		for (String prop : encryptPropNames) {
 			if (propName.equals(prop)) {
@@ -35,4 +40,9 @@ public class EncryptPropHolderConfigurer extends PropertyPlaceholderConfigurer {
 		}
 		return false;
 	}
+
+	public static String get(String name) {
+		return propertiesMap.get(name);
+	}
+	
 }
