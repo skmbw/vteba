971b909e035b345676227481de1ded9572fd96fe
skmbw <tongku2008@126.com>
2014-06-28 19:37:30 +0800

    将spring security的配置以及代码抽取出来

--- vteba/src/main/java/com/vteba/user/service/impl/AuthoritiesServiceImpl.java
+++ vteba/src/main/java/com/vteba/user/service/impl/AuthoritiesServiceImpl.java
@@ -1,8 +1,13 @@
 package com.vteba.user.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.vteba.security.spi.AuthoritiesService;
 import com.vteba.service.generic.impl.GenericServiceImpl;
 import com.vteba.tx.hibernate.IHibernateGenericDao;
 import com.vteba.user.dao.IAuthoritiesDao;
@@ -10,7 +15,7 @@ import com.vteba.user.model.Authorities;
 import com.vteba.user.service.IAuthoritiesService;
 
 @Named
-public class AuthoritiesServiceImpl extends GenericServiceImpl<Authorities, Long> implements IAuthoritiesService {
+public class AuthoritiesServiceImpl extends GenericServiceImpl<Authorities, Long> implements IAuthoritiesService, AuthoritiesService {
 
 	public AuthoritiesServiceImpl() {
 		super();
@@ -29,6 +34,29 @@ public class AuthoritiesServiceImpl extends GenericServiceImpl<Authorities, Long
 
 	public IAuthoritiesDao getAuthoritiesDaoImpl() {
 		return authoritiesDaoImpl;
+	}
+
+	public List<String> getAllAuthorities() {
+		List<String> authList = new ArrayList<String>();
+		String hql = "select a.authName from Authorities a where a.enabled = 1";
+		authList = authoritiesDaoImpl.hqlQueryForList(hql, String.class);
+		return authList;
+	}
+	
+	public List<String> getResourceByAuthName(String authName){
+		List<String> authList = new ArrayList<String>();
+		StringBuilder sb = new StringBuilder();
+		sb = sb.append(" select a.resource_url from auth_resource a, authorities c ");
+		sb = sb.append(" where a.id = c.auth_id and ");
+		sb = sb.append(" c.auth_name = ? ");
+		authList = authoritiesDaoImpl.sqlQueryForList(sb.toString(), String.class, authName);
+		return authList;
+	}
+
+	@Override
+	public List<String> getMethodByAuthName(String authName) {
+		// TODO Auto-generated method stub
+		return Collections.emptyList();
 	}
 	
 
